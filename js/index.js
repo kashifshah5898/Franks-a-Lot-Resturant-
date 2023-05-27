5bb8a3fb70787f06c0ec2ff8647b6f206f1430ca
kashif <kashifshah5898@gmail.com>
2023-05-27 10:55:12 +0500

    update content

--- js/index.js
+++ js/index.js
@@ -11,31 +11,31 @@ const allRecipes = [
             "id": "Special1",
             "link": "https://source.unsplash.com/400x305/?Soup",
             "itemTitle": "Soup",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "Homemade creamy soup with your choice of fillings",
             "price": "BHD 2.992"
         },
         {
             "id": "Special2",
             "link": "https://source.unsplash.com/400x305/?Sandwich",
             "itemTitle": "Sandwich",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "4 oz fajita chicken or beef grilled and sandwiched in hoagie bread with onions, bell peppers, cheese, lettuce and tomato. Served with fries & coleslaw",
             "price": "BHD 2.992"
         },
         {
             "id": "Special3",
             "link": "https://source.unsplash.com/400x305/?Coffee",
             "itemTitle": "Coffee",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "Indulge in the rich and aromatic coffee from our restaurant. Our carefully selected beans are expertly brewed to perfection, delivering a velvety-smooth texture and a well-balanced flavor profile. Whether you're craving a classic espresso or a creamy latte, our coffee will satisfy your taste buds and leave you craving for more.",
             "price": "BHD 1.02"
         },
         {
             "id": "Special4",
             "link": "https://source.unsplash.com/400x305/?Pasta",
             "itemTitle": "Pasta",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", "price": "BHD 2.992"
+            "cardText": "2 crispy flour tortillas filled with choice of meat, cheddar cheese, lettuce, tomatoes. served with fries, salsa. sour cream, pico-de-gallo and guacamole", "price": "BHD 2.992"
         }, {
             "id": "Special5",
-            "link": "https://source.unsplash.com/400x305/?Noodles", "itemTitle": "Noodles", "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", "price": "BHD 2.992"
+            "link": "https://source.unsplash.com/400x305/?Noodles", "itemTitle": "Noodles", "cardText": "Savor the delectable noodles from our restaurant that will transport you to the streets of Asia. Each bite is a harmonious blend of flavors and textures, with our noodles cooked to perfection. From traditional favorites to innovative creations, our noodle dishes offer a delightful culinary adventure that will leave you wanting to slurp every last strand.", "price": "BHD 2.992"
         },
         ]
     }, {
@@ -44,33 +44,33 @@ const allRecipes = [
             "id": "APPETIZERS1",
             "link": "https://source.unsplash.com/400x305/?Platter",
             "itemTitle": "Franks Fiest Platter",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "Experience the ultimate feast with our tantalizing platter at our restaurant. Bursting with a variety of flavors and textures, our platter showcases a selection of delectable dishes that cater to every palate. From succulent meats to fresh seafood and flavorful sides, our platter is a culinary delight that will leave you satisfied and craving for more.",
             "price": "BHD 2.992"
         },
         {
             "id": "APPETIZERS2",
             "link": "https://source.unsplash.com/400x305/?Chips",
             "itemTitle": "Corn Tortilla Chips With Salsa",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "Treat your taste buds to a burst of flavor with our irresistible chips and salsa at our restaurant. Our freshly made crispy chips perfectly complement the zesty and tangy salsa, creating a tantalizing combination. Each bite delivers a delightful crunch and a savory kick, making it a must-have appetizer for every meal.",
             "price": "BHD 2.992"
         },
         {
             "id": "APPETIZERS3",
             "link": "https://source.unsplash.com/400x305/?Cheese",
             "itemTitle": "Cheese fries",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "Indulge in the ultimate comfort food experience with our mouthwatering cheese fries at our restaurant. Crispy golden fries smothered in a generous layer of melted cheese create a heavenly combination that will satisfy your cravings. Each bite offers a perfect harmony of savory, cheesy goodness that will leave you wanting more.",
             "price": "BHD 1.02"
         },
         {
             "id": "APPETIZERS4",
             "link": "https://source.unsplash.com/400x305/?Chicken",
             "itemTitle": "Chicken Fingers",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", "price": "BHD 2.992"
+            "cardText": "Savor the juicy and flavorful chicken fingers from our restaurant. Made with premium cuts of chicken, each finger is carefully seasoned, breaded, and cooked to perfection. With a crispy exterior and tender, moist interior, our chicken fingers are a delightful treat that will satisfy your cravings for delicious, handheld comfort food.", "price": "BHD 2.992"
         }, {
             "id": "APPETIZERS5",
             "link": "https://source.unsplash.com/400x305/?Bbq",
             "itemTitle": "BBQ",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", "price": "BHD 2.992"
+            "cardText": "Experience the tantalizing flavors of BBQ at our restaurant. Our skilled chefs meticulously prepare and slow-cook the finest meats, infusing them with a mouthwatering blend of smoky and savory BBQ goodness. From tender ribs to succulent pulled pork, each bite is a savory delight that will transport your taste buds to BBQ bliss.", "price": "BHD 2.992"
         },
         ]
     }, {
@@ -79,14 +79,14 @@ const allRecipes = [
             "id": "SOUPS1",
             "link": "https://source.unsplash.com/400x305/?Soup",
             "itemTitle": "Creamy Mushroom Soup",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "Indulge in the creamy and comforting delight of our mushroom soup at our restaurant. Made with the finest mushrooms and a velvety base, each spoonful is a burst of earthy flavors and creamy goodness. This soul-warming soup is the perfect appetizer to savor and will leave you craving for more with every spoonful.",
             "price": "BHD 2.992"
         },
         {
             "id": "SOUPS2",
             "link": "https://source.unsplash.com/400x305/?Sandwich",
             "itemTitle": "Enchilada Soup",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "Experience the vibrant and flavorful enchilada soup at our restaurant. This tantalizing soup combines the richness of tomato broth with the perfect blend of spices, tender chicken, and hearty vegetables. Topped with cheese and crispy tortilla strips, each spoonful offers a delicious burst of Mexican-inspired flavors that will leave you craving for more.",
             "price": "BHD 2.992"
         }
         ]
@@ -96,14 +96,14 @@ const allRecipes = [
             "id": "SALADS1",
             "link": "https://source.unsplash.com/400x305/?Salad",
             "itemTitle": "Taco Salads",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "Introducing our mouthwatering taco salad, a culinary delight that combines the best of Tex-Mex flavors in one vibrant dish. Crisp lettuce, seasoned ground beef, zesty salsa, creamy guacamole, and a sprinkle of cheddar cheese dance together, offering a tantalizing explosion of taste with every bite. Experience taco bliss at our restaurant today!",
             "price": "BHD 2.992"
         },
         {
             "id": "SALADS2",
             "link": "https://source.unsplash.com/400x305/?Avocado",
             "itemTitle": "Shrimp & Avacado Salad",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "Indulge in the exquisite flavors of our shrimp and avocado salad, a perfect blend of freshness and decadence. Succulent shrimp, ripe avocado slices, crisp mixed greens, and a tangy citrus dressing create a symphony of tastes that will leave your palate craving for more. Dive into this delightful culinary masterpiece at our restaurant.",
             "price": "BHD 2.992"
         }
         ]
@@ -113,7 +113,7 @@ const allRecipes = [
             "id": "STEAK1",
             "link": "https://source.unsplash.com/400x305/?Steak",
             "itemTitle": "Steak Burrito",
-            "cardText": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "cardText": "Savor the rich and savory delight of our steak burrito, a true carnivore's dream come true. Tender, juicy steak grilled to perfection, wrapped in a warm flour tortilla along with flavorful rice, beans, cheese, and a medley of fresh toppings. Prepare for a burst of bold flavors that will leave you fully satisfied. Experience burrito bliss at our restaurant today!",
             "price": "BHD 2.992"
         }
         ]
